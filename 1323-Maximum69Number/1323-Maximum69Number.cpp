a9a348141906364859df7d8e915e115bffbc2ac5
Samyagya Goel <144522701+Samyagya@users.noreply.github.com>
2026-09-09 11:11:39 +0530

    Create 1323-Maximum69Number

---
+++ 1323-Maximum69Number/1323-Maximum69Number.cpp
@@ -0,0 +1,14 @@
+// Last updated: 09/09/2026, 11:11:38
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string s = to_string(num);
+        for(int i = 0 ; i < s.size() ; i++){
+            if(s[i] == '6'){
+                s[i] = '9';
+                break;
+            }
+        }
+        return stoi(s);
+    }
+};
